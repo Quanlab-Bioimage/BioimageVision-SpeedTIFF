b963a1e4db6860d68308a05565a1be5b719d3f80
FateUBW0227 <FateUBW@mail.ustc.edu.cn>
2025-01-10 17:43:27 +0800

    add demo

---
+++ demo_script/compare_load.py
@@ -0,0 +1,44 @@
+import cpptiff
+from libtiff import TIFF
+import tifffile
+import os
+import time
+from DecTIFFIO import DecReadTifClass
+
+
+if __name__ == '__main__':
+    type = 0
+    path = r'./test_data_for_read'
+    if type == 0:  ### read time for Dec-Tiff
+        obj = DecReadTifClass()
+        print('StartRead')
+        frame = 153   # 153 for 10G, 291 for 20G, 408 for 30G, 532 for 40G, 762 for 60G, 990 for 80G.
+        s1 = time.time()
+        for i in range(frame):
+            obj.AddPath(os.path.join(path, str(i) + '.tif'))
+        for i in range(frame):
+            # print('GetImg: ', name)
+            img = obj.GetImg()
+        print('Dec-Tiff TotalTime: ', time.time() - s1)
+    elif type == 1:  ### read time for Cpp-Tiff
+        s1 = time.time()
+        i_start = 0
+        i_end = 153
+        for i in range(i_start, i_end):
+            im = cpptiff.read_tiff(os.path.join(path, str(i) + '.tif'))
+        print('Cpp-Tiff TotalTime: ', time.time() - s1)
+    elif type == 2:  ### read time for Tifffile
+        i_start = 0
+        i_end = 153
+        s1 = time.time()
+        for i in range(i_start, i_end):
+            tifffile.imread(os.path.join(path, str(i) + '.tif'))
+        print('Tifffile TotalTime: ', time.time() - s1)
+    elif type == 3:  ### read time for Libtiff
+        i_start = 0
+        i_end = 153
+        start = s1.time()
+        for i in range(i_start, i_end):
+            tif = TIFF.open(os.path.join(path, str(i) + '.tif'), mode='r')
+            images = [image for image in tif.iter_images()]
+        print('Libtiff TotalTime: ', time.time() - s1)
---
+++ demo_script/compare_save.py
@@ -0,0 +1,47 @@
+import cpptiff
+from libtiff import TIFF
+import tifffile
+import os
+import time
+from DecTIFFIO.DecWriteTif import DecWriteClass
+
+
+if __name__ == '__main__':
+    imgNumber = 105
+    img = tifffile.imread('./test_data_for_read/0.tif')
+    type = 0
+    if type == 0:  ### write time for Dec-Tiff
+        savePath ='./Dec-Tiff'
+        imgLs = []
+        for i in range(img_number):
+            imgLs.append(img)
+        s1 = time.time()
+        for ii in range(imgNumber):
+            writeObj.AddPath(os.path.join(savePath, str(ii).zfill(5) + '.tif'), imgLs[ii])
+        for ii in range(imgNumber):
+            add = writeObj.WriteOk()
+            name = os.path.basename(add)
+        print('Dec-Tiff TotalTime: ', time.time() - s1)
+        writeObj.Close()
+    elif type == 1:   ### write time for Cpp-Tiff
+        savePath = './Cpp-Tiff'
+        s1 = time.time()
+        for ii in range(imgNumber):
+            name = os.path.join(savePath, str(ii).zfill(5) + '.tif')
+            cpptiff.write_tiff(name, img, compression="lzw")
+        print('Cpp-Tiff TotalTime: ', time.time() - s1)
+    elif type == 2:   ### write time for Tifffile
+        savePath = './Tifffile'
+        s1 = time.time()
+        for ii in range(imgNumber):
+            tifffile.imwrite(os.path.join(savePath, str(ii).zfill(5) + '.tif'), img, compression='lzw')
+        print('Tifffile TotalTime: ', time.time() - s1)
+    elif type == 3:   ### write time for Libtiff
+        savePath = './Libtiff'
+        s1 = time.time()
+        for ii in range(imgNumber):
+            tif = TIFF.open(name, mode='w')
+            tif.write_image(img, compression='lzw', write_rgb=False)
+            tif.close()
+        print('Libtiff TotalTime: ', time.time() - s1)
+

ecceaaf7249172e3e77f3970ca011c46e1647ce5
FateUBW0227 <FateUBW@mail.ustc.edu.cn>
2025-01-10 19:10:33 +0800

    update

--- demo_script/compare_save.py
+++ demo_script/compare_save.py
@@ -12,8 +12,10 @@ if __name__ == '__main__':
     type = 0
     if type == 0:  ### write time for Dec-Tiff
         savePath ='./Dec-Tiff'
+        os.makedirs(savePath)
         imgLs = []
-        for i in range(img_number):
+        writeObj = DecWriteClass()
+        for i in range(imgNumber):
             imgLs.append(img)
         s1 = time.time()
         for ii in range(imgNumber):
@@ -25,6 +27,7 @@ if __name__ == '__main__':
         writeObj.Close()
     elif type == 1:   ### write time for Cpp-Tiff
         savePath = './Cpp-Tiff'
+        os.makedirs(savePath)
         s1 = time.time()
         for ii in range(imgNumber):
             name = os.path.join(savePath, str(ii).zfill(5) + '.tif')
@@ -32,15 +35,17 @@ if __name__ == '__main__':
         print('Cpp-Tiff TotalTime: ', time.time() - s1)
     elif type == 2:   ### write time for Tifffile
         savePath = './Tifffile'
+        os.makedirs(savePath)
         s1 = time.time()
         for ii in range(imgNumber):
             tifffile.imwrite(os.path.join(savePath, str(ii).zfill(5) + '.tif'), img, compression='lzw')
         print('Tifffile TotalTime: ', time.time() - s1)
     elif type == 3:   ### write time for Libtiff
         savePath = './Libtiff'
+        os.makedirs(savePath)
         s1 = time.time()
         for ii in range(imgNumber):
-            tif = TIFF.open(name, mode='w')
+            tif = TIFF.open(os.path.join(savePath, str(ii).zfill(5) + '.tif'), mode='w')
             tif.write_image(img, compression='lzw', write_rgb=False)
             tif.close()
         print('Libtiff TotalTime: ', time.time() - s1)
